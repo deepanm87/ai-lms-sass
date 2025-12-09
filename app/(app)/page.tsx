6d9476987c8aa1f3a0f89b4020edabf12be90bb6
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-12-09 15:54:05 -0500

    setup course card component and a lib constants file and added sample course data

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Header } from "@/components/Header"
+import { CourseCard } from "@/components/courses/CourseCard"
 import {
   ArrowRight,
   Play,
