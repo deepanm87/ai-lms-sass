dcf3d0eef8b44cd3ca160626732e1689ea1a614b
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-12-09 14:00:09 -0500

    setup sanity schema types and updated structure

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,10 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import type { SchemaTypeDefinition } from 'sanity'
+import { categoryType } from './categoryType'
+import { courseType } from './courseType'
+import { lessonType } from './lessonType'
+import { moduleType } from './moduelType'
+import { noteType } from './noteType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [categoryType, courseType, lessonType, moduleType, noteType]
 }
